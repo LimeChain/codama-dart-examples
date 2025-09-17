bde08ec8953583c274c5eb1091410b531a9f6e82
ERoydev <e.roydev@gmail.com>
2025-09-17 16:18:24 +0300

    improve read examples

--- multi-program-example/dart-project/bin/data_structures/read/read_enum.dart
+++ multi-program-example/dart-project/bin/data_structures/read/read_enum.dart
@@ -2,59 +2,47 @@ import 'package:solana/solana.dart';
 import '../constants.dart';
 import '../../../clients/dart/generated/anchor_data_structures/lib.dart';
 
+// This example demonstrates how to access enum variants generated as sealed classes in Dart.
+// You can use `is` checks and direct property access to work with the data.
 
-void printExampleEnum(ExampleEnum enumValue, String prefixMessage) {
-  if (enumValue is VariantA) {
-    print('${prefixMessage}: VariantA(value0: ${enumValue.value0}, value1: ${enumValue.value1})');
-  } else if (enumValue is VariantB) {
-    print('${prefixMessage}: VariantB(x: ${enumValue.x}, y: ${enumValue.y})');
-  } else if (enumValue is VariantC) {
-    print('${prefixMessage}: VariantC');
-  } else if (enumValue is VariantD) {
-    print('${prefixMessage}: VariantD(a: ${enumValue.value0}, b: ${enumValue.value1}, c: ${enumValue.value2})');
-  } else if (enumValue is VariantE) {
-    print('${prefixMessage}: VariantE(values: ${enumValue.nested}, flag: ${enumValue.flag})');
-    print('Struct Values - name: ${enumValue.nested.name}, age: ${enumValue.nested.age}, location: ${enumValue.nested.location}');
-  } else if (enumValue is VariantF) {
-    print('${prefixMessage}: VariantF(value0: ${enumValue.value0})');
-  } else if (enumValue is VariantG) {
-    print('${prefixMessage}: VariantG(value0: ${enumValue.value0})');
-  } else if (enumValue is VariantH) {
-    print('${prefixMessage}: VariantH(value0: ${enumValue.value0})');
-  } else if (enumValue is VariantI) {
-    print('${prefixMessage}: VariantI(a: ${enumValue.a}, b: ${enumValue.b}, c: ${enumValue.c}, d: ${enumValue.d})');
-  } else {
-    print('Unknown variant');
-  }
-}
-
-// Result: Reading of the array from Blockchain State works as expected for these fields -> Proof Of Deserialization 
-void readEnum(RpcClient client, Ed25519HDKeyPair payer) async {
-  final systemProgram = Ed25519HDPublicKey.fromBase58(SystemProgram.programId);
-
-  final Ed25519HDKeyPair enumAcc = await Ed25519HDKeyPair.fromMnemonic(
-    enumMnemonic,
-  );
-
+Future<EnumAccount> readEnum(RpcClient client, Ed25519HDKeyPair payer) async {
+  final Ed25519HDKeyPair enumAcc = await Ed25519HDKeyPair.fromMnemonic(enumMnemonic);
   final enumAccount = await EnumAccount.fetch(client, enumAcc.publicKey);
 
-  print('\n========================');
-  printExampleEnum(enumAccount.my_enum, 'My Enum');
-  printExampleEnum(enumAccount.optional_enum!, 'Optional Enum');
-  for (var i = 0; i < enumAccount.enum_vec.length; i++) {
-    printExampleEnum(enumAccount.enum_vec[i], 'Enum Vec[$i]');
-  }
-  for (var i = 0; i < enumAccount.optional_enum_vec!.length; i++) {
-    printExampleEnum(enumAccount.optional_enum_vec![i], 'Optional Enum Vec[$i]');
-  }
-  for (var i = 0; i < enumAccount.enum_array.length; i++) {
-    printExampleEnum(enumAccount.enum_array[i], 'Enum Array[$i]');
+  // Accessing a simple enum field
+  final ExampleEnum myEnum = enumAccount.my_enum;
+  if (myEnum is VariantA) {
+    // Access fields specific to VariantA
+    print('VariantA: value0=${myEnum.value0}, value1=${myEnum.value1}');
+  } else if (myEnum is VariantB) {
+    print('VariantB: x=${myEnum.x}, y=${myEnum.y}');
   }
 
-  printExampleEnum(enumAccount.struct_with_enum.field_enum, 'Struct with Enum - field_enum');
-  for (var i = 0; i < enumAccount.struct_with_enum.field_enum_vec.length; i++) {
-    printExampleEnum(enumAccount.struct_with_enum.field_enum_vec[i], 'Struct with Enum - field_enum_vec[$i]');
+  // Accessing an optional enum
+  final ExampleEnum? optionalEnum = enumAccount.optional_enum;
+  if (optionalEnum != null && optionalEnum is VariantC) {
+    print('OptionalEnum is VariantC');
   }
-  printExampleEnum(enumAccount.struct_with_optional_enum.field_optional_enum!, 'Struct with Optional Enum - field_optional_enum');
-  print('========================');
+
+  // Accessing enums in a vector
+  for (final e in enumAccount.enum_vec) {
+    if (e is VariantD) {
+      print('EnumVec element is VariantD: a=${e.value0}, b=${e.value1}, c=${e.value2}');
+    }
+  }
+
+  // Accessing enums in an array
+  for (final e in enumAccount.enum_array) {
+    if (e is VariantE) {
+      print('EnumArray element is VariantE: flag=${e.flag}');
+    }
+  }
+
+  // Accessing enums inside a struct
+  final ExampleEnum structEnum = enumAccount.struct_with_enum.field_enum;
+  if (structEnum is VariantF) {
+    print('Struct field_enum is VariantF: value0=${structEnum.value0}');
+  }
+
+  return enumAccount;
 }
--- multi-program-example/dart-project/bin/data_structures/read/read_struct.dart
+++ multi-program-example/dart-project/bin/data_structures/read/read_struct.dart
@@ -3,7 +3,7 @@ import '../../../clients/dart/generated/anchor_data_structures/lib.dart';
 import '../constants.dart';
 
 // Result: Reading of the array from Blockchain State works as expected for these fields -> Proof Of Deserialization 
-void readStruct(RpcClient client, Ed25519HDKeyPair payer) async {
+Future<StructAccount> readStruct(RpcClient client, Ed25519HDKeyPair payer) async {
   final systemProgram = Ed25519HDPublicKey.fromBase58(SystemProgram.programId);
 
   final Ed25519HDKeyPair structAcc = await Ed25519HDKeyPair.fromMnemonic(
@@ -11,87 +11,6 @@ void readStruct(RpcClient client, Ed25519HDKeyPair payer) async {
   );
 
   final structAccount = await StructAccount.fetch(client, structAcc.publicKey);
-  print('Simple Struct Account - \n'
-    'name: ${structAccount.simple_struct.name}, \n'
-    'age: ${structAccount.simple_struct.age}, \n'
-    'location: ${structAccount.simple_struct.location}, \n'
-  );
-
-  print('Primitive Account - \n'
-    'u8_field: ${structAccount.primitive_account.u8_field}, \n'
-    'i8_field: ${structAccount.primitive_account.i8_field}, \n'
-    'u16_field: ${structAccount.primitive_account.u16_field}, \n'
-    'i16_field: ${structAccount.primitive_account.i16_field}, \n'
-    'u32_field: ${structAccount.primitive_account.u32_field}, \n'
-    'i32_field: ${structAccount.primitive_account.i32_field}, \n'
-    'u64_field: ${structAccount.primitive_account.u64_field}, \n'
-    'i64_field: ${structAccount.primitive_account.i64_field}, \n'
-    'bool_field: ${structAccount.primitive_account.bool_field}, \n'
-    'pubkey_field: ${structAccount.primitive_account.pubkey_field} \n'
-    'string_field: ${structAccount.primitive_account.string_field} \n'
-  );
-
-  print('String Account - \n'
-    'simple_string: ${structAccount.string_account.simple_string}, \n'
-    'optional_string: ${structAccount.string_account.optional_string}, \n'
-  );
-
-  print('Vectors Account - \n'
-    'u8_vec: ${structAccount.vector_account.u8_vec}, \n'
-    'i16_vec: ${structAccount.vector_account.i64_vec}, \n'
-    'string_vec: ${structAccount.vector_account.string_vec}, \n'
-    'pubkey_vec: ${structAccount.vector_account.pubkey_vec}, \n'
-  );
-
-  print('Optional Account - \n'
-    'optional_u32: ${structAccount.optional_account.optional_u32}, \n'
-    'optional_vec: ${structAccount.optional_account.optional_vec}, \n'
-    'optional_struct: ${structAccount.optional_account.optional_struct != null ? structAccount.optional_account.optional_struct!.name : null}, \n'
-  );
-
-  print('Struct Acc - \n'
-    'simple_struct: ${structAccount.nested_structs.simple_struct}, \n'
-    '   Simple Struct - a: ${structAccount.nested_structs.simple_struct.a}, b: ${structAccount.nested_structs.simple_struct.b} \n'
-    'nested_vec_struct: ${structAccount.nested_structs.nested_vec_struct}, \n'
-    '   Nested Vec Struct - ${structAccount.nested_structs.nested_vec_struct.map((e) => 'a: ${e.a}, b: ${e.b}').join(' | ')} \n'
-    'optional_nested: ${structAccount.nested_structs.optional_nested} \n'
-    '   Optional Nested - a: ${structAccount.nested_structs.optional_nested!.a}, b: ${structAccount.nested_structs.optional_nested!.b} \n'
-  );
-
-  print('Fixed Array Account - \n'
-    'u8_array: ${structAccount.fixed_array_account.u8_array}, \n'
-    'i32_array: ${structAccount.fixed_array_account.i32_array}, \n'
-    'pubkey_array: ${structAccount.fixed_array_account.pubkey_array}, \n'
-  );
-
-
-  if (structAccount.simple_enum is VariantA) {
-    final v = structAccount.simple_enum as VariantA;
-    print('simple_enum: VariantA(value0: ${v.value0}, value1: ${v.value1})');
-  } else if (structAccount.simple_enum is VariantB) {
-    final v = structAccount.simple_enum as VariantB;
-    print('simple_enum: VariantB(x: ${v.x}, y: ${v.y})');
-  } else if (structAccount.simple_enum is VariantD) {
-    final v = structAccount.simple_enum as VariantD;
-    print('simple_enum: VariantD(value0: ${v.value0}, value1: ${v.value1}, value2: ${v.value2})');
-  } else if (structAccount.simple_enum is VariantE) {
-    final v = structAccount.simple_enum as VariantE;
-    print('simple_enum: VariantE(nested: (name: ${v.nested.name}, age: ${v.nested.age}, location: ${v.nested.location}), flag: ${v.flag})');
-  } else if (structAccount.simple_enum is VariantF) {
-    final v = structAccount.simple_enum as VariantF;
-    print('simple_enum: VariantF(value0: ${v.value0})');
-  } else if (structAccount.simple_enum is VariantG) {
-    final v = structAccount.simple_enum as VariantG;
-    print('simple_enum: VariantG(value0: ${v.value0})');
-  } else if (structAccount.simple_enum is VariantH) {
-    final v = structAccount.simple_enum as VariantH;
-    print('simple_enum: VariantH(value0: ${v.value0})');
-  } else if (structAccount.simple_enum is VariantI) {
-    final v = structAccount.simple_enum as VariantI;
-    print('simple_enum: VariantI(a: ${v.a}, b: ${v.b} c: ${v.c}, d: ${v.d})');
-  } else if (structAccount.simple_enum is VariantC) {
-    print('simple_enum: VariantC()');
-  } else {
-    print('Unknown enum variant');
-  }
+  // Same you can just access the struct fields directly
+  return structAccount;
 }
