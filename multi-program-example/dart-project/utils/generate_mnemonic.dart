f15a6c91731d965485fe5dfed402a79cd6ce477d
ERoydev <e.roydev@gmail.com>
2025-09-17 16:11:55 +0300

    fix: Script keypair folder handling

--- multi-program-example/dart-project/utils/generate_mnemonic.dart
+++ multi-program-example/dart-project/utils/generate_mnemonic.dart
@@ -7,6 +7,11 @@ Functionality to generate all required mnemonics to test the example project.
 
 Future<String> generateAndSaveMnemonic(String mnemonicName) async {
   final dir = 'keypairs';
+  // Ensure the directory exists
+  final directory = Directory(dir);
+  if (!await directory.exists()) {
+    await directory.create(recursive: true);
+  }
   // Generates a new mnemonic and saves it to a file named by mnemonicName inside dir
   final mnemonic = bip39.generateMnemonic();
   final path = '$dir/$mnemonicName';
